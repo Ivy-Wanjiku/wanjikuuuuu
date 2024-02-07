95b69963eee617ed06f07fce89e497d5a8db1202
ivy <wanjiruwanjikuivy@gmail.com>
2024-02-07 07:05:47 +0300

    add one file

---
+++ Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+  var name = "Ivy Wanjiku"
+    println(name)
+  var age = 19
+  println(age)
+  var weight = 62.3
+  println(weight)
+  var number = +254734567890
+  println(number)
+  var nationality = "kenyan"
+  println(nationality)
+  val citizen = true
+  val  notCitizen = false
+  println(citizen)
+}
